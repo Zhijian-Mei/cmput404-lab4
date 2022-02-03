83df3319c0118e60af371a12970dde943d6dee78
Zhijian Mei <46204799+Zhijian-Mei@users.noreply.github.com>
2022-02-04 02:09:23 +0800

    Update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
 from django.http import HttpResponse, HttpResponseRedirect
 
 from django.urls import reverse
@@ -10,6 +5,7 @@ from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
 
 from .models import Question,Choice
+from django.views import generic
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -41,3 +37,21 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
